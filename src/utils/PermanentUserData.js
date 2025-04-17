eefc885cba1e56b546ccaa4995c69fe7e3748833
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-04-17 23:13:30 +0800

    feat: add log

--- src/utils/PermanentUserData.js
+++ src/utils/PermanentUserData.js
@@ -4,32 +4,38 @@ import {
   readDeleteSet,
   writeDeleteSet,
   createDeleteSet,
-  DSEncoderV1, DSDecoderV1, ID, DeleteSet, YArrayEvent, Transaction, Doc // eslint-disable-line
-} from '../internals.js'
+  DSEncoderV1,
+  DSDecoderV1,
+  ID,
+  DeleteSet,
+  YArrayEvent,
+  Transaction,
+  Doc, // eslint-disable-line
+} from "../internals.js";
 
-import * as decoding from 'rdlib0/decoding'
+import * as decoding from "rdlib0/decoding";
 
-import { mergeDeleteSets, isDeleted } from './DeleteSet.js'
+import { mergeDeleteSets, isDeleted } from "./DeleteSet.js";
 
 export class PermanentUserData {
   /**
    * @param {Doc} doc
    * @param {YMap<any>} [storeType]
    */
-  constructor (doc, storeType = doc.getMap('users')) {
+  constructor(doc, storeType = doc.getMap("users")) {
     /**
      * @type {Map<string,DeleteSet>}
      */
-    const dss = new Map()
-    this.yusers = storeType
-    this.doc = doc
+    const dss = new Map();
+    this.yusers = storeType;
+    this.doc = doc;
     /**
      * Maps from clientid to userDescription
      *
      * @type {Map<number,string>}
      */
-    this.clients = new Map()
-    this.dss = dss
+    this.clients = new Map();
+    this.dss = dss;
     /**
      * @param {YMap<any>} user
      * @param {string} userDescription
@@ -38,32 +44,58 @@ export class PermanentUserData {
       /**
        * @type {YArray<Uint8Array>}
        */
-      const ds = user.get('ds')
-      const ids = user.get('ids')
-      const addClientId = /** @param {number} clientid */ clientid => this.clients.set(clientid, userDescription)
-      ds.observe(/** @param {YArrayEvent<any>} event */ event => {
-        event.changes.added.forEach(item => {
-          item.content.getContent().forEach(encodedDs => {
-            if (encodedDs instanceof Uint8Array) {
-              this.dss.set(userDescription, mergeDeleteSets([this.dss.get(userDescription) || createDeleteSet(), readDeleteSet(new DSDecoderV1(decoding.createDecoder(encodedDs)))]))
-            }
-          })
-        })
-      })
-      this.dss.set(userDescription, mergeDeleteSets(ds.map(encodedDs => readDeleteSet(new DSDecoderV1(decoding.createDecoder(encodedDs))))))
-      ids.observe(/** @param {YArrayEvent<any>} event */ event =>
-        event.changes.added.forEach(item => item.content.getContent().forEach(addClientId))
-      )
-      ids.forEach(addClientId)
-    }
+      const ds = user.get("ds");
+      const ids = user.get("ids");
+      const addClientId = /** @param {number} clientid */ (clientid) =>
+        this.clients.set(clientid, userDescription);
+      ds.observe(
+        /** @param {YArrayEvent<any>} event */ (event) => {
+          event.changes.added.forEach((item) => {
+            item.content.getContent().forEach((encodedDs) => {
+              if (encodedDs instanceof Uint8Array) {
+                this.dss.set(
+                  userDescription,
+                  mergeDeleteSets([
+                    this.dss.get(userDescription) || createDeleteSet(),
+                    readDeleteSet(
+                      new DSDecoderV1(decoding.createDecoder(encodedDs))
+                    ),
+                  ])
+                );
+              }
+            });
+          });
+        }
+      );
+      this.dss.set(
+        userDescription,
+        mergeDeleteSets(
+          ds.map((encodedDs) =>
+            readDeleteSet(new DSDecoderV1(decoding.createDecoder(encodedDs)))
+          )
+        )
+      );
+      ids.observe(
+        /** @param {YArrayEvent<any>} event */ (event) =>
+          event.changes.added.forEach((item) =>
+            item.content.getContent().forEach(addClientId)
+          )
+      );
+      ids.forEach(addClientId);
+    };
     // observe users
-    storeType.observe(event => {
-      event.keysChanged.forEach(userDescription =>
-        initUser(storeType.get(userDescription), userDescription)
-      )
-    })
+    storeType.observe((event) => {
+      event.keysChanged.forEach((userDescription) => {
+        let user = storeType.get(userDescription);
+        if (!user) {
+          console.error("get user failed:" + userDescription);
+        } else {
+          initUser(user, userDescription);
+        }
+      });
+    });
     // add initial data
-    storeType.forEach(initUser)
+    storeType.forEach(initUser);
   }
 
   /**
@@ -73,69 +105,76 @@ export class PermanentUserData {
    * @param {Object} conf
    * @param {function(Transaction, DeleteSet):boolean} [conf.filter]
    */
-  setUserMapping (doc, clientid, userDescription, { filter = () => true } = {}) {
-    const users = this.yusers
-    let user = users.get(userDescription)
+  setUserMapping(doc, clientid, userDescription, { filter = () => true } = {}) {
+    const users = this.yusers;
+    let user = users.get(userDescription);
     if (!user) {
-      user = new YMap()
-      user.set('ids', new YArray())
-      user.set('ds', new YArray())
-      users.set(userDescription, user)
+      user = new YMap();
+      user.set("ids", new YArray());
+      user.set("ds", new YArray());
+      users.set(userDescription, user);
     }
-    user.get('ids').push([clientid])
-    users.observe(_event => {
+    user.get("ids").push([clientid]);
+    users.observe((_event) => {
       setTimeout(() => {
-        const userOverwrite = users.get(userDescription)
+        const userOverwrite = users.get(userDescription);
         if (userOverwrite !== user) {
           // user was overwritten, port all data over to the next user object
           // @todo Experiment with Y.Sets here
-          user = userOverwrite
+          user = userOverwrite;
           // @todo iterate over old type
           this.clients.forEach((_userDescription, clientid) => {
             if (userDescription === _userDescription) {
-              user.get('ids').push([clientid])
+              user.get("ids").push([clientid]);
             }
-          })
-          const encoder = new DSEncoderV1()
-          const ds = this.dss.get(userDescription)
+          });
+          const encoder = new DSEncoderV1();
+          const ds = this.dss.get(userDescription);
           if (ds) {
-            writeDeleteSet(encoder, ds)
-            user.get('ds').push([encoder.toUint8Array()])
+            writeDeleteSet(encoder, ds);
+            user.get("ds").push([encoder.toUint8Array()]);
           }
         }
-      }, 0)
-    })
-    doc.on('afterTransaction', /** @param {Transaction} transaction */ transaction => {
-      setTimeout(() => {
-        const yds = user.get('ds')
-        const ds = transaction.deleteSet
-        if (transaction.local && ds.clients.size > 0 && filter(transaction, ds)) {
-          const encoder = new DSEncoderV1()
-          writeDeleteSet(encoder, ds)
-          yds.push([encoder.toUint8Array()])
-        }
-      })
-    })
+      }, 0);
+    });
+    doc.on(
+      "afterTransaction",
+      /** @param {Transaction} transaction */ (transaction) => {
+        setTimeout(() => {
+          const yds = user.get("ds");
+          const ds = transaction.deleteSet;
+          if (
+            transaction.local &&
+            ds.clients.size > 0 &&
+            filter(transaction, ds)
+          ) {
+            const encoder = new DSEncoderV1();
+            writeDeleteSet(encoder, ds);
+            yds.push([encoder.toUint8Array()]);
+          }
+        });
+      }
+    );
   }
 
   /**
    * @param {number} clientid
    * @return {any}
    */
-  getUserByClientId (clientid) {
-    return this.clients.get(clientid) || null
+  getUserByClientId(clientid) {
+    return this.clients.get(clientid) || null;
   }
 
   /**
    * @param {ID} id
    * @return {string | null}
    */
-  getUserByDeletedId (id) {
+  getUserByDeletedId(id) {
     for (const [userDescription, ds] of this.dss.entries()) {
       if (isDeleted(ds, id)) {
-        return userDescription
+        return userDescription;
       }
     }
-    return null
+    return null;
   }
 }
